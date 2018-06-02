3a3b4db7f7900ed2e9a04f37e2c705b309781ac0
Kunihiko Sato <ksato@laxus.co>
2018-06-02 20:22:09 +0900

    Add consts

--- src/consts/common_consts.go
+++ src/consts/common_consts.go
@@ -6,7 +6,7 @@ const BASIC_AUTH_PASS = "user-name:jedbLCxb5XcRi"
 
 const ENV_PRODUCTION = 1
 const PATH_TEST = RELATIVE_PATH + "resources/config_test.json"
-const PATH_PRODUCTION = RELATIVE_PATH + "resources/config_production.json"
+const PATH_PRODUCTION = "./config.json"
 
 const PARAM_DELETED_NO = "0"
 const PARAM_DELETED_YES = "1"
